d53a8466fd7702b8492bf2b8a39a770a97179102
sehoon <gsh723@naver.com>
2021-08-23 22:41:35 +0900

    end backend

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 5002;
+const port = 5003;
 
 const config = require("./config/key");
 
@@ -9,6 +9,9 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+const { auth } = require("./middleware/auth");
 const mongoose = require("mongoose");
 mongoose.connect(config.mongoURI,
     {
@@ -27,10 +30,11 @@ app.get('/', (req, res) => {
     res.send('Hello World! hello kangsehoon');
 });
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     // 1. client에서 정보 가져온다
     // 2. 정보들을 데이터베이스에 넣는다.
     const user = new User(req.body);
+
     user.save((err, userInfo) => {
         if (err) return res.json({
             success: "false",
@@ -40,7 +44,63 @@ app.post('/register', (req, res) => {
             success: true
         })
     });
+})
 
+app.post('/api/users/login', (req, res) => {
+    console.log(1);
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user) {
+            console.log(2);
+            return res.json({
+                success: false,
+                message: "get out here!"
+            })
+        }
+        console.log(5);
+        user.comparePassword(req.body.password, (err, isMatched) => {
+            console.log(3);
+            if (!isMatched) {
+                return res.json({ loginSuccess: false, message: "not matched password!" });
+            }
+            // 비밀번호가 맞았으니까 토큰 생성!!
+            user.generateToken((err, user) => {
+                console.log(4);
+                if (err) return res.status(400).json({
+                    status: 'error',
+                    error: err,
+                });
+
+                // 토큰을 저장한다. -> cookie or sessionstorage or localstorage
+                res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({
+                        loginSuccess: true,
+                        userId: user._id
+                    })
+            })
+        })
+    })
+});
+
+app.get("/api/users/auth", auth, (req, res) => {
+    res.status(200).json({
+        _id: req.user._id,
+        inAdmin: req.user.role === 0 ? false : true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+    User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).send({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
--- models/User.js
+++ models/User.js
@@ -1,3 +1,7 @@
+const bycrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
+
 
 // 1. import mongoose 
 const mongoose = require("mongoose");
@@ -13,7 +17,7 @@ const userSchema = mongoose.Schema({
         trim: true,
         unique: 1
     },
-    Password: {
+    password: {
         type: String,
         minlength: 5
     },
@@ -34,8 +38,62 @@ const userSchema = mongoose.Schema({
     }
 });
 
-// 3. wrap by model and export
+userSchema.pre('save', function (next) {
+    const user = this;
 
+    if (user.isModified('password')) {
+        // 암호화       
+        bycrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) {
+                return next(err)
+            };
+            bycrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err);
+                user.password = hash;
+                next();
+            });
+        });
+    }
+    else {
+        next();
+    }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, callback) {
+    console.log("cp1");
+    bycrypt.compare(plainPassword, this.password, function (err, isMatched) {
+        if (err) return callback(err),
+            console.log("cp2");
+        callback(null, isMatched);
+    });
+}
+
+userSchema.methods.generateToken = function (callback) {
+
+    const user = this;
+    // jwt 이용해서 토큰 생성
+    const token = jwt.sign(user._id.toHexString(), 'secretToken');
+    user.token = token;
+    user.save(function (err, user) {
+        if (err) return callback(err);
+        callback(null, user);
+    })
+
+};
+
+userSchema.statics.findByToken = function (token, callback) {
+    const user = this;
+
+    // 1. 토큰 decode
+    jwt.verify(token, 'secretToken', function (err, decoded) {
+        user.findOne({ "_id": decoded, "token": token }, function (err, user) {
+            if (err) return callback(err);
+            callback(null, user);
+        })
+    })
+}
+
+// 3. wrap by model and export
 const User = mongoose.model('User', userSchema);
 
 module.exports = { User };
