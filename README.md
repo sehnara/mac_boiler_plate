933389cab00d38f7e2ce43d6ca8dc780c60c3153
sehoon <gsh723@naver.com>
2021-08-23 13:50:39 +0900

    set security of datas

---
+++ README.md
@@ -0,0 +1,1 @@
+# mac_boiler_plate
--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const port = 5002;
 
+const config = require("./config/key");
+
+const { User } = require("./models/User");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://sehoon:abcd1234@boilerplate.7ltub.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+mongoose.connect(config.mongoURI,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -17,10 +24,25 @@ mongoose.connect("mongodb+srv://sehoon:abcd1234@boilerplate.7ltub.mongodb.net/my
 })
 
 app.get('/', (req, res) => {
-    res.send('Hello World! hello');
+    res.send('Hello World! hello kangsehoon');
 });
+
+app.post('/register', (req, res) => {
+    // 1. client에서 정보 가져온다
+    // 2. 정보들을 데이터베이스에 넣는다.
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
+        if (err) return res.json({
+            success: "false",
+            "message": err
+        })
+        return res.status(200).json({
+            success: true
+        })
+    });
+
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 });
-
